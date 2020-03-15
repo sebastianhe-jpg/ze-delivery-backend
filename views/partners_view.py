2196f7fe5eee271f0b5619f1168ae9f907dafc13
sebastianhe-jpg <sbastian.he@gmail.com>
2020-03-14 22:48:31 -0300

    WIP: partners logic, get, create, search, distance from point to polygon

--- views/partners_view.py
+++ views/partners_view.py
@@ -5,6 +5,9 @@ from geojson import MultiPolygon
 from mods import input_validator_module
 from mods import sanitize_input_module
 from mods import formatting_module
+from mods import mongo_module
+from shapely.geometry import Point as xpoint
+from shapely.geometry import Polygon
 
 
 def init():
@@ -21,32 +24,26 @@ def partner_create(request):
     :param request:
     :return:
     """
-    if input_validator_module.validate_keys(request, ['id', 'tradingName', 'ownerName',
-                                                      'document', 'coverageArea', 'address']):
-        return formatting_module.output_format('missing keys'), 415
+    invalid, reason = input_validator_module.validate_keys(request, ['id', 'tradingName', 'ownerName',
+                                                             'document', 'coverageArea', 'address'], {"id": int})
+    if invalid:
+        return formatting_module.output_format(invalid, reason), 415
 
-    id = sanitize_input_module(request.json['id'])
-    trading_name = sanitize_input_module(request.json['tradingName'])
-    owner_name = sanitize_input_module(request.json['ownerName'])
-    document = sanitize_input_module(request.json['document'])
-    coverageArea = sanitize_input_module(request.json['coverageArea'])
-    address = sanitize_input_module(request.json['address'])
-
-    MultiPolygon([
-        [[[30, 20], [45, 40], [10, 40], [30, 20]]],
-        [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
-    ])
-    Point([-46.57421, -21.785741])
-
+    id = sanitize_input_module.entry_clean(request.json['id'])
+    trading_name = sanitize_input_module.entry_clean(request.json['tradingName'])
+    owner_name = sanitize_input_module.entry_clean(request.json['ownerName'])
+    document = sanitize_input_module.entry_clean(request.json['document'])
+    coverageArea = request.json['coverageArea']
+    address = request.json['address']
     partner = {
         "id": id,
-        "tradingName": trading_name,
-        "ownerName": owner_name,
-        "document": document, # uuid
-        "coverageArea": MultiPolygon(coverageArea),
-        "address": Point(address)
+        "trading_name": trading_name,
+        "owner_name": owner_name,
+        "document": document,
+        "coverageArea": MultiPolygon(coverageArea['coordinates']),
+        "address": Point(address['coordinates'])
     }
-
+    mongo_module.mongo_insert(partner)
     output, code = '200', 200
     return formatting_module.output_format(output), code
 
@@ -57,12 +54,14 @@ def partner_get(request):
     :param request:
     :return:
     """
-    if input_validator_module.validate_keys(request, ['id']):
-        return formatting_module.output_format('missing keys'), 415
-    id = sanitize_input_module(request.json['id'])
-
+    invalid, reason = input_validator_module.validate_keys(request, ['id'], {"id": int})
+    if invalid:
+        return formatting_module.output_format(invalid, reason), 415
+    id = sanitize_input_module.entry_clean(request.json['id'])
+    id = {"id": id}
+    a = mongo_module.mongo_find(id, single=True)
     output, code = '200', 200
-    return formatting_module.output_format(output), code
+    return formatting_module.output_format(output,a), code
 
 
 def partner_search(request):
@@ -71,10 +70,29 @@ def partner_search(request):
     :param request:
     :return:
     """
-    if input_validator_module.validate_keys(request, ['lng', 'lat']):
-        return formatting_module.output_format('missing keys'), 415
-    lng = sanitize_input_module(request.json['lng'])
-    lat = sanitize_input_module(request.json['lat'])
+    invalid, reason = input_validator_module.validate_keys(request, ['lng', 'lat'], {'lng': int, 'lat': int})
+    if invalid:
+        return formatting_module.output_format(invalid, reason), 415
+    lng = sanitize_input_module.entry_clean(request.json['lng'])
+    lat = sanitize_input_module.entry_clean(request.json['lat'])
+    partners_data = mongo_module.mongo_find({})
 
     output, code = '200', 200
-    return formatting_module.output_format(output), code
+    closest = find_closest_partner(lng, lat, partners_data)
+    return formatting_module.output_format(output, closest), code
+
+
+def find_closest_partner(lng, lat, partners_data):
+    point = xpoint(lng, lat)
+    distance = {}
+    for partner in partners_data:
+        for coordinates_array in partner['coverageArea']['coordinates']:
+            for coordinates in coordinates_array:
+                if partner['id'] in distance:
+                    dist = distance[partner['id']]
+                    dist.append(point.distance(Polygon(coordinates)))
+                    distance[partner['id']] = dist
+                else:
+                    distance[partner['id']] = [point.distance(Polygon(coordinates))]
+    key = min(distance, key=distance.get)
+    return next((partner for partner in partners_data if partner['id'] == key), None)
