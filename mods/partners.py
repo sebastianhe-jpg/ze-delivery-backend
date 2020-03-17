07916777f1b4e7fdbc5109118a24187c743ca110
unknown <she@cognitiva.la>
2020-03-17 15:14:28 -0300

    ADD: partners class added, logic changes

--- mods/partners.py
+++ mods/partners.py
@@ -3,26 +3,93 @@
 """
 partners class module
 """
+from shapely.geometry import Point
+from shapely.geometry import Polygon
+from mods import mongo_module
 
 
 class Partner:
-    def __init__(self):
+    """
+    test
+    """
+    def __init__(self, partner=None, partner_id=None, lng=None, lat=None):
         self.debug = True
+        self.partner = partner if partner else None
+        self.partner_id = partner_id if partner_id else None
+        self.lng = lng if lng else None
+        self.lat = lat if lat else None
 
-    def partner_create(self, partner):
-        self.id = partner['id']
-        self.trading_name = partner['trading_name']
-        self.owner_name = partner['owner_name']
-        self.document = partner['document']
-        self.coverageArea = partner['coverageArea']
-        self.address = partner['address']
-        return
+    def partner_create(self):
+        """
+        test
+        :param partner:
+        :return:
+        """
+        try:
+            mongo_module.mongo_insert(self.partner)
+            output = 'sucesfully created'
+            code = 201
+        except Exception as err:
+            output = str(err)
+            code = 409
+        return output, code
 
-    def partner_get(self, id):
-        self.id = id
-        return
+    def partner_get(self):
+        """
+        test
+        :param id:
+        :return:
+        """
+        try:
+            document = mongo_module.mongo_find(self.partner_id, single=True)
+            output = 'partner' if document else 'No data match'
+            code = 200 if document else 204
+        except Exception as err:
+            document = None
+            output = str(err)
+            code = 400
+        return output, document, code
 
-    def partner_search(self, lng, lat):
-        self.lng = lng
-        self.lat = lat
-        return
+    def partner_search(self):
+        """
+        test
+        :param lng:
+        :param lat:
+        :return:
+        """
+        point = Point(self.lng, self.lat)
+        try:
+            partners_data = mongo_module.mongo_find({})
+            closest = self.partner_find_closest(point, partners_data) if partners_data else None
+            output = 'nearest partner' if partners_data else 'No data match'
+            code = 200 if partners_data else 204
+        except Exception as err:
+            closest = None
+            output = str(err)
+            code = 400
+        return output, closest, code
+
+    def partner_find_closest(self, point, partners_data):
+        """
+        set point from lnt, lat. iter over documents from partners_data
+        and get distancec fron point.
+        finally get min distance fron partners_data and return lower value
+        :param lng: double.
+        :param lat: double
+        :param partners_data: array of objects
+        :return:
+        """
+        distance = {}
+        for partner in partners_data:
+            if 'coverageArea' in partner and 'coordinates' in partner['coverageArea']:
+                for coordinates_array in partner['coverageArea']['coordinates']:
+                    for coordinates in coordinates_array:
+                        if partner['id'] in distance:
+                            dist = distance[partner['id']]
+                            dist.append(point.distance(Polygon(coordinates)))
+                            distance[partner['id']] = dist
+                        else:
+                            distance[partner['id']] = [point.distance(Polygon(coordinates))]
+        key = min(distance, key=distance.get) if distance else None
+
+        return next((partner for partner in partners_data if partner['id'] == key), None)
--- views/partners_view.py
+++ views/partners_view.py
@@ -5,13 +5,11 @@ partners views module
 """
 from geojson import MultiPolygon
 from geojson import Point
-from shapely.geometry import Polygon
-from shapely.geometry import Point as xpoint
 from mods import input_validator_module
 from mods import sanitize_input_module
 from mods import formatting_module
-from mods import mongo_module
-
+# from mods import mongo_module
+from mods.partners import Partner
 
 def init():
     """
@@ -52,13 +50,8 @@ def partner_create(request):
         "address": Point(address['coordinates'])
     }
 
-    try:
-        mongo_module.mongo_insert(partner)
-        output = 'sucesfully created'
-        code = 201
-    except Exception as err:
-        output = str(err)
-        code = 409
+    partner = Partner(partner)
+    output, code = partner.partner_create()
     return formatting_module.output_format(output), code
 
 
@@ -75,14 +68,9 @@ def partner_get(request):
         return formatting_module.output_format(reason), 415
     partner_id = sanitize_input_module.entry_clean(request.json['id'])
     partner_id = {"id": partner_id}
-    try:
-        document = mongo_module.mongo_find(partner_id, single=True)
-        output = 'partner' if document else 'No data match'
-        code = 200 if document else 204
-    except Exception as err:
-        document = None
-        output = str(err)
-        code = 400
+
+    partner = Partner(partner_id=partner_id)
+    output, document, code = partner.partner_get()
     return formatting_module.output_format(output, document), code
 
 
@@ -102,39 +90,6 @@ def partner_search(request):
     lng = sanitize_input_module.entry_clean(request.json['lng'])
     lat = sanitize_input_module.entry_clean(request.json['lat'])
 
-    try:
-        partners_data = mongo_module.mongo_find({})
-        closest = find_closest_partner(lng, lat, partners_data) if partners_data else None
-        output = 'nearest partner' if partners_data else 'No data match'
-        code = 200 if partners_data else 204
-    except Exception as err:
-        closest = None
-        output = str(err)
-        code = 400
-    return formatting_module.output_format(output, closest), code
-
-
-def find_closest_partner(lng, lat, partners_data):
-    """
-    set point from lnt, lat. iter over documents from partners_data
-    and get distancec fron point.
-    finally get min distance fron partners_data and return lower value
-    :param lng: double.
-    :param lat: double
-    :param partners_data: array of objects
-    :return:
-    """
-    point = xpoint(lng, lat)
-    distance = {}
-    for partner in partners_data:
-        if 'coverageArea' in partner and 'coordinates' in partner['coverageArea']:
-            for coordinates_array in partner['coverageArea']['coordinates']:
-                for coordinates in coordinates_array:
-                    if partner['id'] in distance:
-                        dist = distance[partner['id']]
-                        dist.append(point.distance(Polygon(coordinates)))
-                        distance[partner['id']] = dist
-                    else:
-                        distance[partner['id']] = [point.distance(Polygon(coordinates))]
-    key = min(distance, key=distance.get)
-    return next((partner for partner in partners_data if partner['id'] == key), None)
+    partner = Partner(lng=lng, lat=lat)
+    output, data, code = partner.partner_search()
+    return formatting_module.output_format(output, data), code
